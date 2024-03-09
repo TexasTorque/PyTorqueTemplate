3880718a7cb70a61a4a24b3d1d9bd7a3c7c1f524
humandavey <davey.adams.three@gmail.com>
2024-03-09 12:47:15 -0600

    Update robot functions

--- robot.py
+++ robot.py
@@ -2,5 +2,4 @@ from lib.robot.TorqueRobot import TorqueRobotBase
 import systems
 
 class Robot(TorqueRobotBase):
-    def __init__(self) -> None:
-        systems.initialize()
+    pass
--- systems.py
+++ systems.py
@@ -4,10 +4,18 @@ from lib.subsystem.TorqueSubsystem import Subsystem
 
 subsystems: list[Subsystem] = []
 
-def initialize() -> None:
+def add_subsystems() -> None:
     # Add subsystems here
     subsystems.append(Empty())
     subsystems.append(Input())
 
     for subsystem in subsystems:
         subsystem.set_subsystems(subsystems)
+
+def init_subsystems() -> None:
+    for subsystem in subsystems:
+        subsystem.initialize()
+
+def update_subsystems() -> None:
+    for subsystem in subsystems:
+        subsystem.update()

f391067facac79a9481674a78b9bd227dd5ebcdd
humandavey <davey.adams.three@gmail.com>
2024-03-26 17:01:19 -0500

    Update template

--- systems.py
+++ systems.py
@@ -1,21 +1,5 @@
-from input import Input
-from subsystems.empty import Empty
-from lib.subsystem.TorqueSubsystem import Subsystem
+import input as _input
+import subsystems.empty as _empty
 
-subsystems: list[Subsystem] = []
-
-def add_subsystems() -> None:
-    # Add subsystems here
-    subsystems.append(Empty())
-    subsystems.append(Input())
-
-    for subsystem in subsystems:
-        subsystem.set_subsystems(subsystems)
-
-def init_subsystems() -> None:
-    for subsystem in subsystems:
-        subsystem.initialize()
-
-def update_subsystems() -> None:
-    for subsystem in subsystems:
-        subsystem.update()
+inputsystem = _input.get_instance()
+emptysystem = _empty.get_instance()
